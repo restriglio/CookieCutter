5f028a1a8d5f7a67016ae64e2fe4926cece5ddca
Raul Striglio <raul.striglio@gmail.com>
2018-02-01 12:55:18 -0300

    Add comments and javadoc

--- globantsdk/src/main/java/com/example/modelviewviewmodel/repository/UseCaseRepository.java
+++ globantsdk/src/main/java/com/example/modelviewviewmodel/repository/UseCaseRepository.java
@@ -8,24 +8,37 @@ import java.util.List;
 
 /**
  * Created by raul.striglio on 27/11/17.
+ *
+ * @typeparam T This represents your model entity
+ *
  */
 
 public abstract class UseCaseRepository<T> {
 
 
     protected Context mContext;
+
+    /* LiveData to manage a list of T elements */
     protected LiveData<List<T>> mDataList = new MutableLiveData<>();
+
+    /* LiveData to manage a T element */
+    protected LiveData<T> mData = new MutableLiveData<>();
 
     public UseCaseRepository(Context context){
         mContext = context;
         initLocalData();
     }
 
+    /* Initialize and configure local Room database  */
     public abstract void initLocalData();
-    public abstract void addData(T t);
-    public abstract void addDataList(List<T> dataList);
-    public abstract void requestDataToServer();
 
+    /* Add a T element into the databse */
+    public abstract void addData(T t);
+
+    /* Add a list of T into the database */
+    public abstract void addDataList(List<T> dataList);
+
+    /* method that returns the local LiveData list. This method must be observed in your view */
     public LiveData<List<T>> getDataList() {
         return mDataList;
     }
@@ -33,4 +46,8 @@ public abstract class UseCaseRepository<T> {
     public void setDataList(LiveData<List<T>> mDataList) {
         this.mDataList = mDataList;
     }
+
+    /* Implements the logic to request the data to the server with RxAndroid */
+    public abstract void requestDataToServer();
+
 }
